83b85e26251ffd3b1e726347fcb917131ca5334b
abdann <abdann2009@hotmail.com>
2022-06-09 10:56:35 -0500

    Migrated project to 2.0

--- main.py
+++ main.py
@@ -7,31 +7,34 @@ import cogs.commanderrorhandler
 import cogs.moderationhandler
 import cogs.shitpostinghandler
 import os
+import asyncio
 from pretty_help import PrettyHelp
 
 class PolBot(commands.Bot):
     async def on_ready(self):
         print(f'{self.user.name} has connected to Discord!')
 
-def main():
+async def main():
     from dotenv import load_dotenv
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN_LIVE')
     intents = discord.Intents.default()
     intents.members = True
+    intents.message_content = True
     bot = PolBot(
         command_prefix='.pol ',
         intents=intents,
         help_command=PrettyHelp()
     )
     bot.servers = cogs.serverhandler.ServerConfigHandler()
-    bot.add_cog(cogs.permissionshandler.PermissionsHandler(bot))
-    bot.add_cog(cogs.reactionhandler.ReactionHandler(bot))
-    bot.add_cog(cogs.commanderrorhandler.CommandErrHandler(bot))
-    bot.add_cog(cogs.moderationhandler.ModerationHandler(bot))
-    bot.add_cog(cogs.shitpostinghandler.ShitpostingHandler(bot))
+    await bot.add_cog(cogs.permissionshandler.PermissionsHandler(bot))
+    await bot.add_cog(cogs.reactionhandler.ReactionHandler(bot))
+    await bot.add_cog(cogs.commanderrorhandler.CommandErrHandler(bot))
+    await bot.add_cog(cogs.moderationhandler.ModerationHandler(bot))
+    await bot.add_cog(cogs.shitpostinghandler.ShitpostingHandler(bot))
     #Add cogs to bot before this line
-    bot.run(TOKEN)
+    async with bot:
+        await bot.start(TOKEN)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
